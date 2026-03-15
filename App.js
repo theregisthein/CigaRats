71bdd1c50f079f83e0ca74fd3503138f16821826
theregisthein <rinaldiregisthein@gmail.com>
2026-03-15 02:13:49 -0300

    feat: adiciona calendario traduzido e ajustes de layout na home

--- App.js
+++ App.js
@@ -1,19 +1,49 @@
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DarkTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Ionicons } from '@expo/vector-icons';
 import HomeScreen from './src/Screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={DarkTheme}>
       <Stack.Navigator>
-        <Stack.Screen 
-          name="Home" 
-          component={HomeScreen} 
-          options={{ title: 'Início' }} 
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: 'Bem Vindo ao CigaRats',
+
+            headerLeft: () => (
+              <Ionicons
+                name="list-outline"
+                size={24}
+                color="yellow"
+                style={{ marginRight: 15 }}
+                onPress={() => alert('Botão pressionado!')}
+              />
+            ),
+
+            headerRight: () => (
+              <Ionicons
+                name="settings-outline"
+                size={24}
+                color="yellow"
+                style={{ marginRight: 15 }}
+                onPress={() => alert('Botão pressionado!')}
+              />
+            ),
+          }}
+
+          
+
         />
+
+
+
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,12 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Calendar, LocaleConfig } from 'react-native-calendars';
 
-// Uma tela não precisa criar o Stack, ela apenas exibe o conteúdo
+
+LocaleConfig.locales['pt-br'] = {
+  monthNames: [
+    'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+    'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+  ],
+  monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+  dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+  dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
+  today: 'Hoje'
+};
+
+LocaleConfig.defaultLocale = 'pt-br';
+
+
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Bem-vindo ao CigaRats!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: '#000' }}>
+
+      <View style={styles.container}>
+        <Image style={styles.image}
+          source={require('../../assets/image/User-2.jpg')}
+        />
+
+        <Text style={{ fontSize: 24, color: 'white', padding: 10, textAlign: 'center' }}>Bem Vindo ao Cigarats!</Text>
+
+        <View style={styles.diaryData}>
+          <Text style={{ fontSize: 12, color: 'white', lineHeight:35, margin: 10,textAlign: 'center' }}>{"Último cigarro\n 2 h atrás"}</Text>
+          <Text style={{ fontSize: 12, color: 'white', lineHeight: 35, margin: 10,textAlign: 'center'  }}>{"Cigarros fumados\n12"}</Text>
+          <Text style={{ fontSize: 12, color: 'white', lineHeight: 35, margin: 10,textAlign: 'center' }}>{"Meta diária\n15"}</Text>
+        </View>
+
+        <View style={styles.calendar}>
+          <Calendar
+            firstDay={0}
+            theme={{
+              backgroundColor: '#da2020',
+              calendarBackground: '#313131',
+              textSectionTitleColor: '#fff',
+              selectedDayBackgroundColor: '#ff0000',
+              todayTextColor: '#ff0000',
+              dayTextColor: '#fff',
+              monthTextColor: '#fff',
+              width: '300',
+            }}
+            onDayPress={day => {
+              console.log('Dia selecionado', day);
+            }}
+          />
+        </View>
+
+       
+
+      </View>
+
+      
+
     </View>
   );
 }
@@ -14,8 +65,30 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 50,
+  },
+
+  image: {
+    width: '40%',
+    height: '20%',
     alignItems: 'center',
     justifyContent: 'center',
+    borderRadius: 100,
   },
+
+  diaryData: {
+    flexDirection: 'row',
+    padding: 10,
+    gap: 15,
+  },
+
+  calendar: {
+    width: '90%',
+    marginTop: 10,
+    borderRadius: 10,
+    overflow: 'hidden',
+  },
+
 });
